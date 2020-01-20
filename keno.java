57d1173823babc54e2fa0d1ea972aa929af06f10
connor-roach <connor.roach@gmail.com>
2020-01-20 11:43:02 -0500

    Update keno.java

--- keno.java
+++ keno.java
@@ -16,11 +16,13 @@ public class keno {
 		System.out.println("| $$\\  $$ | $$      | $$\\  $$$| $$  | $$");
 		System.out.println("| $$ \\  $$| $$$$$$$$| $$ \\  $$|  $$$$$$/");
 		System.out.println("|__/  \\__/|________/|__/  \\__/ \\______/ ");
-		System.out.println("\nPlease load your wallet: ");
-		int wallet = Integer.parseInt(scn.nextLine());
 		
+		System.out.print("\nPlease load your wallet: $");
+		int begWallet = Integer.parseInt(scn.nextLine());
+		int wallet = begWallet;
+		//initialize array for the drawing
 		int[] draws = new int[20];
-		
+
 		//Payout array. Each amount represents wiinnings per match [0] is 0 matches
 		int[][] payout = new int[][]{
 			{ 0, 0, 1, 5, 72 }, //4 spot
@@ -34,18 +36,27 @@ public class keno {
 
 	char playAgain = 'Y';
 	while (playAgain == 'Y'){
+		//reset winnings per draw (winnings) and winnings per card (totalWinnings)
+		int winnings = 0;
+		int totalWinnings = 0;		
 
 		// Determine how many spots the player wants
-		System.out.println("How many spots? (Min of 4, Max of 10)");
+		System.out.print("How many spots? (Min of 4, Max of 10): ");
 		int spots = Integer.parseInt(scn.nextLine());	
-	
-		System.out.println("How much to bet?");
+
+		//Determine how many draws the player wants
+		System.out.print("How many draws? (Min of 1, Max of 10): ");
+		int playerDraws = Integer.parseInt(scn.nextLine());
+		
+
+		//take bet amount out of wallet
+		System.out.print("How much to bet? $");
 	       	int bet = Integer.parseInt(scn.nextLine());
-		wallet = wallet - bet;	
+		wallet = wallet - bet*playerDraws;	
 		int[] playerNums = new int[spots];
 		
 		//Player chooses random numbers or their own picks
-		System.out.println("Would you like to pick your own numbers? (Y/N)");
+		System.out.print("Would you like to pick your own numbers? (Y/N): ");
 		char select = scn.nextLine().charAt(0);
 
 		if (select == 'Y'){
@@ -54,67 +65,86 @@ public class keno {
 				playerNums[i] = scn.nextInt();
 			}
 			scn.nextLine();
-			System.out.println("Got it. Playing these numbers: " 
-					+ Arrays.toString(playerNums));
+			System.out.println("\nGot it. Playing these numbers: \n" + Arrays.toString(playerNums));
 		}
 		else if (select == 'N'){
-			
 			for (int i =0; i < spots; i++){
 				int randPick = rand.nextInt(80) + 1;
 				if(IntStream.of(playerNums).anyMatch(n -> n == randPick) == false){
 					playerNums[i] = randPick;
 				}
 			}
-			System.out.println("Picking for you. Your numbers are:" 
-					+ Arrays.toString(playerNums));
+			System.out.println("\nPicking for you. Your numbers are:\n" + Arrays.toString(playerNums));
 		}
 		else {
 			System.out.println("You did not enter either Y or N. Goodbye");
 		}
-		System.out.println("Here is the draw:");
-			//Generate the 20 numbers to match to 
-		for (int i =0; i < 20; i++){
-			int randNum = rand.nextInt(80) + 1;
-			if(IntStream.of(draws).anyMatch(y -> y == randNum) == false){
-				draws[i] = randNum;
-				System.out.print(draws[i] + " ");
-			}
-					
-		}
-
-		//match the numbers
-		System.out.println();
-
-		int matchCount = 0;
-		for (int i = 0; i < spots; i++){
-			//the lambda thing needs this final thing for some reason idk
-			final int idk = playerNums[i];
-			
-			if(IntStream.of(draws).anyMatch(z -> z == idk)== true){
-				System.out.println(playerNums[i] + " is a match!");
-				matchCount++;
-			}
-			else {
-				System.out.println(playerNums[i] + " is not a match.");
-			}
-		}
-		System.out.println("You got " + matchCount + " number(s)");
 		
-		int winnings = payout[spots-4][matchCount] * bet;
-		System.out.println("You win $" + winnings);
-		wallet = wallet + winnings;
-		System.out.println("Your wallet balance is " + wallet);
+		//generate the draw
+		int drawNum = 0;
+		while (	playerDraws > 0 ){
+			drawNum++;
+			System.out.println("\nHere is draw #" + drawNum);
+			int generated = 0;
+			while (generated < 20){
+				int randNum = rand.nextInt(80) + 1;
+				if(IntStream.of(draws).anyMatch(y -> y == randNum) == false){
+					draws[generated] = randNum;
+					System.out.print(draws[generated] + " ");
+					generated++;
+					if (generated == 10)
+						System.out.println();
+				}
+						
+			}
+
+			//match the numbers
+			System.out.println("\n");
+
+			int matchCount = 0;
+			for (int i = 0; i < spots; i++){
+				//the lambda thing needs this final thing for some reason idk
+				final int idk = playerNums[i];
+				
+				if(IntStream.of(draws).anyMatch(z -> z == idk)== true){
+					System.out.println(playerNums[i] + " is a match!");
+					matchCount++;
+				}
+				else {
+					System.out.println(playerNums[i] + " is not a match.");
+				}
+			}
+			System.out.println("\nYou got " + matchCount + " number(s)");
+			
+			//get winnings amount and add it to the wallet
+			winnings = payout[spots-4][matchCount] * bet;
+			System.out.println("You win $" + winnings);
+			totalWinnings = totalWinnings + winnings;
+			playerDraws--;
+		}
+		wallet = wallet + totalWinnings;
+		System.out.println("Your wallet balance is $" + wallet);
 
 		if (wallet <= 0){
-			System.out.println("You ran out of money");
+			System.out.println("\nYou ran out of money");
 			playAgain = 'N';
 		}
 		else { 
-			System.out.println("Play again? (Y/N)");
+			System.out.print("\nPlay again? (Y/N): ");
 			playAgain = scn.nextLine().charAt(0);
 		}
 		
 	}
+	System.out.println("\nThanks for playing.");
+	if (wallet - begWallet > 0){
+		System.out.println("You won $" + (wallet-begWallet));
+	}
+	else if (wallet - begWallet < 0){
+		System.out.println("You lost $" + (begWallet-wallet));
+	}
+	else {
+		System.out.println("You broke even");
+	}	
 	}
 }
 
